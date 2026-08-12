9db3aa13a846046dcdf2ab559c554e446e970e35
ahmed45alotttibi-gif <ahmed45alotttibi@gmail.com>
2026-08-12 10:43:49 +0300

    Change bot token in index.js
    Updated the bot token for client login.

--- index.js
+++ index.js
@@ -206,4 +206,4 @@ client.on('interactionCreate', async (interaction) => {
   }
 });
 
-client.login("MTUzNjU5NjMwNzc1MjUyMTczOA.GCyiaX.DNh_1UInAcPvz_nI8LN6waUFB3m8kJedm9t7fM");
+client.login("MTUzNjU5NjMwNzc1MjUyMTczOA.GreVrk.Tu_taEBCCAu5Glk-G_lUyMg9EfsBBXWoHmG4ao");
